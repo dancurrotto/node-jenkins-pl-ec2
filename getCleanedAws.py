26684d763b9177f7ce6f5861e869c42a7940952d
Dan Currotto <Dan@ValueSourceConsulting.com>
2018-09-24 20:16:16 -0400

    Moving to nginx!

--- getCleanedAws.py
+++ getCleanedAws.py
@@ -1,11 +1,11 @@
 import sys
 def cleanAws():
     inputValue =  sys.argv[1]
-    return inputValue.replace('"','').replace('\\','')
-
+    cleaned = inputValue.replace('"','').replace('\\','')
+    f = open("cleanedAwsValue", "w")
+    f.write(str(cleaned))
+    f.close
+    return str(cleaned)
 
 if __name__ == '__main__':
     sys.stdout.write(cleanAws())
-
-    
-    
